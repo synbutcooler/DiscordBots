74855cc6b50fecce9b643cd180bf0303b366ccb3
synbutcooler <bakirbutkovic6@gmail.com>
2026-03-07 18:54:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 
-STICKIED_TOKEN = os.environ.get('STICKIED_TOKEN')
-MONGODB_URI = os.environ.get('MONGODB_URI')
-DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
-
-DATA_DIR = 'data'
-if not os.path.exists(DATA_DIR):
-    os.makedirs(DATA_DIR)
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+STICKIED_BOT_TOKEN = os.environ.get("STICKIED_BOT_TOKEN")
+DISCORD_KEY_API_SECRET = os.environ.get("DISCORD_KEY_API_SECRET")
