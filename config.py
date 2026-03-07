be748fec38caca2589d0d5691a6240edc71e7bd8
synbutcooler <bakirbutkovic6@gmail.com>
2026-03-07 10:29:43 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+STICKIED_TOKEN = os.environ.get('STICKIED_TOKEN')
+MONGODB_URI = os.environ.get('MONGODB_URI')
+DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
+
+DATA_DIR = 'data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+PLUGINS_FILE = os.path.join(DATA_DIR, 'plugins_data.json')
